1ce6a3b9f7b9563dd4919acceb62483c70f6a901
Luckman333 <zengaja@gmx.de>
2023-03-07 14:18:16 +0100

    Update site.config.ts
    changed notion page and basic contact information

--- site.config.ts
+++ site.config.ts
@@ -2,27 +2,27 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
-
+  rootNotionPageId: 'Inversion-Of-Control-ea937a6991d94d5db3dd78cb1f8819af',
+https://universal-jackrabbit-a4f.notion.site/Inversion-Of-Control-ea937a6991d94d5db3dd78cb1f8819af
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Inversion Of Control - Metal',
+  domain: 'www.inversionofcontrol.de',
+  author: 'Thomas Lagemann',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Home of the Darmstadt based metal formation Inversion of control',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  instagram: 'inversionofcontrolband',
+  github: 'Luckman333',
+  facebook: 'InversionOfControlBand',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: '@inversionofcontrol9925', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
